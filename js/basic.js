0cd58b3b9c9c4c105eeb72c71ca5c84c5a1d4400
lanver <lanveer@qq.com>
2017-12-24 16:16:39 +0800

    init the project

---
+++ js/basic.js
@@ -0,0 +1,130 @@
+/**
+ * Created by Administrator on 2017/12/15.
+ */
+
+
+$(function () {
+    $('.nav-detail-box').eq(0).show();
+    $('.nav>a').click(function (e) {
+        e.preventDefault();
+        $(this).addClass('on').siblings().removeClass('on');
+        var idx= $(this).index();
+        $('.nav-detail-box').hide().eq(idx).show()
+    })
+
+
+//    底部菜单的点击事件
+//     $('.footer>a').click(function () {
+//         $(this).find()
+//     })
+
+
+
+//    充值记录界面
+
+    $('.p-nav>a').eq(0).css({
+        color:'#fb524e',
+        'borderBottom':'1px solid #fb524e'
+    })
+    $('.p-nav a').click(function (e) {
+        e.preventDefault();
+        $(this).css({
+            color:'#fb524e',
+           'borderBottom':'1px solid #fb524e'
+        }).siblings().css({
+            color:'#333',
+            borderBottom:'none'
+        })
+    })
+
+
+//    我的消息弹窗
+    var h= $('.msg-box').height();
+    $('.msg-box').css({
+        'marginTop':-(h/2)+'px'
+    })
+
+    //点击确认按钮
+    $('.msg-confirm').click(function (e) {
+        e.preventDefault();
+        $('#mask,.msg-box').hide();
+    })
+
+    //点击消息列表查看更多
+    $('.p-recordList').click(function (e) {
+        e.preventDefault();
+        $('#mask,.msg-box').show();
+    })
+
+//    点击管理按钮
+    $('.p-edit').click(function () {
+        $('.msg-edit').show();
+        $('.p-edit').hide();
+        $('.delete').show();
+        $('.p-chooseAll,.p-cancel').show();
+    })
+
+//    点击选择
+    $('.msg-edit').click(function () {
+        $(this).css({
+          backgroundPosition:'42% 33%'
+        })
+    })
+
+//    点击取消
+    $('.p-cancel').click(function () {
+        $('.msg-edit').hide();
+        $('.p-edit').show();
+        $('.delete').hide();
+        $('.p-chooseAll,.p-cancel').hide()
+    })
+
+//    点击全选
+    $('.p-chooseAll').click(function () {
+        $('.msg-edit').css({
+            backgroundPosition:'42% 33%'
+        })
+    })
+
+
+
+//    充值点击
+    $('.p-c-box>div').click(function () {
+        $(this).css({
+            background:'#ffd258',
+            color:'#fff',
+            border:'1px solid #ffd258'
+        }).siblings().css({
+            background:'none',
+            color:'#333',
+            border:'1px solid #333'
+        })
+    })
+
+
+//    充值方式
+    $('.p-c-tools>span').eq(0).css({
+        color:'#fb524e',
+        'borderBottom':'1px solid #fb524e'
+    })
+
+    $('.p-c-tools>span').click(function () {
+        $(this).css({
+            color:'#fb524e',
+            'borderBottom':'1px solid #fb524e'
+        }).siblings().css({
+            color:'#666',
+            'borderBottom':'none'
+        })
+    })
+
+
+//    点击充值银行
+
+    $('.p-c-tools-content>a').click(function (e) {
+        e.preventDefault();
+        $(this).addClass('bankOn').siblings().removeClass('bankOn');
+        $(this).find('span').addClass('checked').parent().siblings().find('span').removeClass('checked')
+    })
+
+})
---
+++ js/index.js
@@ -0,0 +1,83 @@
+/**
+ * Created by Administrator on 2017/12/21.
+ */
+
+
+
+$(function () {
+    var tempA=[];
+    $('.chooseBallBox>div').click(function () {
+        var objA={};
+        var name=$(this).attr('name');
+        if(name=='chooseBall0' || name=='chooseBall1' || name=='chooseBall6' || name=='chooseBall7'|| name=='chooseBall11'|| name=='chooseBall12'|| name=='chooseBall17'|| name=='chooseBall18'|| name=='chooseBall22'|| name=='chooseBall23' || name=='chooseBall28'|| name=='chooseBall29'|| name=='chooseBall33'|| name=='chooseBall34'|| name=='chooseBall39'|| name=='chooseBall44'|| name=='chooseBall45'){
+            $(this).toggleClass('situation1');
+            var name=$(this).attr('name');
+            var val=$(this).attr('value');
+            tempA.push(objA);
+            console.log(tempA)
+            // for(var i=0 ; i<tempA.length;i++){
+            //     var str2='';
+            //     str2+='<div class="'+tempA[i].name+'">'+tempA[i].val+'</div>'
+            // }
+            // $('.chooseBallResult-mid').append(str2);
+        }else if(name=='chooseBall2' || name=='chooseBall3' || name=='chooseBall8' || name=='chooseBall9'|| name=='chooseBall13'|| name=='chooseBall14'|| name=='chooseBall19'|| name=='chooseBall24'|| name=='chooseBall25'|| name=='chooseBall30' || name=='chooseBall35'|| name=='chooseBall36'|| name=='chooseBall40'|| name=='chooseBall41'|| name=='chooseBall46'|| name=='chooseBall47'){
+            $(this).toggleClass('situation2')
+
+        }else if(name=='chooseBall4' || name=='chooseBall5' || name=='chooseBall10' || name=='chooseBall15'|| name=='chooseBall16'|| name=='chooseBall20'|| name=='chooseBall21'|| name=='chooseBall26'|| name=='chooseBall27'|| name=='chooseBall31' || name=='chooseBall32'|| name=='chooseBall37'|| name=='chooseBall38'|| name=='chooseBall42'|| name=='chooseBall43'|| name=='chooseBall48'){
+            $(this).toggleClass('situation3')
+        }
+    })
+
+//    选号后放入box
+
+    // var tempA=[];
+    $('.chooseBallBoxA>div').click(function () {
+        // var objA={};
+        // var name=$(this).attr('name');
+        // var val=$(this).attr('value');
+        // objA.val=val;
+        // objA.name=name;
+        // tempA.push(objA)
+        // var str='';
+
+        //去除重复的
+        // var arr1 = tempA
+        // var result = [],
+        //     hash = {};
+        //     hash_ = {}
+        // for (var i = 0; i<arr1.length; i++) {
+        //     var elem = arr1[i].val;
+        //     var elem_ = arr1[i].val;
+        //     if (!hash[elem]) {
+        //         if (!hash_[elem_]) {
+        //             result.push(arr1[i]);
+        //             hash_[elem_] = true;
+        //         };
+        //         hash[elem] = true;
+        //     }
+        // }
+        // console.log('result is:',result)
+        //
+        // for(var i=0 ; i<tempA.length;i++){
+        //     var str2='';
+        //     str2+='<div class="'+tempA[i].name+'">'+tempA[i].val+'</div>'
+        // }
+        // $('.chooseBallResult-mid').append(str2);
+        var total=$('.chooseBallResult-mid>div').length;
+        $('.total').html('共'+total+'注')
+
+    })
+
+
+
+
+//    点击删除图标
+    $('.chooseBallResult-left').click(function () {
+        $('.chooseBall').removeClass('situation1')
+        $('.chooseBall').removeClass('situation2')
+        $('.chooseBall').removeClass('situation3')
+        $('.chooseBallResult-mid').empty();
+        $()
+        $('.total').html('共0注')
+    })
+})
